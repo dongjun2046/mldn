798701bde8451b32d79bd66b73dbb1b2017081f8
dongjun <lannoble520@163.com>
2016-08-18 16:17:05 +0800

    change

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Hello{
   public static void main(String args[]){
-		System.out.println("date"+new Date());
+		System.out.println("date111"+new Date());
   }
 }
--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class Hello{
   public static void main(String args[]){
-		System.out.println("世界你好");
+		System.out.println("世界你好1111");
   }
 }
