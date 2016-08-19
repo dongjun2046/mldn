b13f53c3289668d3106c27abb0a82caf6f918b14
dongjun2046 <lannoble520@163.com>
2016-08-19 13:13:12 +0800

    2 update admin.java file

--- Admin.java
+++ Admin.java
@@ -2,5 +2,7 @@ public class Admin{
   public static void main(String args[]){
       private Interger adminid£»
 	  private string password;
+        private Interger flag£»
+	  private string name£»
   }
 }
