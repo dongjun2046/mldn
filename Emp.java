b346a4eb534cba68e10b3ac09d5d49507e96de57
dongjun2046 <lannoble520@163.com>
2016-08-19 11:31:30 +0800

    patch

--- Emp.java
+++ Emp.java
@@ -2,5 +2,6 @@ public class Emy{
   public static void main(String args[]){
       System.out.println("www.dongjun.com");
       System.out.println("yngj");
+      System.out.println("add pacth");
   }
 }
